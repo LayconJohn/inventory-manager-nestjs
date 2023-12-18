afbdc68a4454740a842261fcdd466564c625a690
Laycon John <laycon.gomes@tld.local>
2023-12-18 14:46:16 -0300

    feat(stocks-output): create stock output endpoint

--- src/stocks-output/stocks-output.module.ts
+++ src/stocks-output/stocks-output.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StocksOutputService } from './stocks-output.service';
 import { StocksOutputController } from './stocks-output.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [StocksOutputController],
   providers: [StocksOutputService],
 })
--- src/stocks-output/stocks-output.service.ts
+++ src/stocks-output/stocks-output.service.ts
@@ -1,10 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { CreateStocksOutputDto } from './dto/create-stocks-output.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { NotFoundError } from 'src/errors/index';
 
 @Injectable()
 export class StocksOutputService {
-  create(createStocksOutputDto: CreateStocksOutputDto) {
-    return 'This action adds a new stocksOutput';
+  constructor(
+    private prismaService: PrismaService
+  ) {}
+
+  
+  async create(createStocksOutputDto: CreateStocksOutputDto) {
+    const product = await this.prismaService.stockOutput.findUnique({
+      where: {id: createStocksOutputDto.product_id}
+    });
+    if(!product) {
+      throw new NotFoundError("Product not found");
+    }
+
+    const result = await this.prismaService.$transaction([
+      this.prismaService.stockOutput.create({
+        data: {
+          productId: createStocksOutputDto.product_id,
+          quantity: createStocksOutputDto.quantity,
+          date: createStocksOutputDto.date
+        }
+      }),
+
+      this.prismaService.stockOutput.update({
+        where: {
+          id: createStocksOutputDto.product_id
+        },
+        data: {
+          quantity: {
+            increment: createStocksOutputDto.quantity
+          }
+        }
+      }),
+    ]);
+
+    return result[0];
   }
 
   findAll() {
